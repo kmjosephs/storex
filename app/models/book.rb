25aa45ea82b1c9014a17d9c43eefaa273d77a0d3
Keisha Josephs <kmjosephs@gmail.com>
2018-01-21 18:36:01 -0600

    Added sale model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,10 +1,14 @@
 class Book < ActiveRecord::Base
   extend FriendlyId
   friendly_id :name, use: :slugged
-  
+
   belongs_to :user
+  has_many :sales
   has_attached_file :image
   has_attached_file :resource
+
+  validates_numericality_of :price,
+    greater_than: 49, message: "Must be at least 50 cents"
 
   validates_attachment_content_type :image,
 		content_type:  /^image\/(png|gif|jpeg)/,
